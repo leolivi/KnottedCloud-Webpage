25b412937562ed633f3bfcedc6ee691f5b1f4bdb
Léonie Liviero <leolivi@me.com>
2024-02-14 18:32:28 +0100

    daily commit + upgrade style

--- assets/javascript/buttons.js
+++ assets/javascript/buttons.js
@@ -1,13 +1,24 @@
 // Scroll down button
 const scrollDown = document.querySelector(".teaser");
 
-scrollDown.addEventListener("click", () =>
-  window.scrollTo({
-    // how far to scroll:
-    top: 950,
-    behavior: "smooth",
-  })
-);
+scrollDown.addEventListener("click", () => {
+  // wenn Bildschirmbreite grösser als 500 oder breiter als 600
+  if (window.innerWidth >= 500) {
+    window.scrollTo({
+      // how far to scroll:
+      top: 950,
+      behavior: "smooth",
+    })
+  } else {
+    window.scrollTo({
+      // how far to scroll wenn Bildschirm kleiner als 500:
+      top: 700,
+      behavior: "smooth",
+    })
+  }
+});
+  
+
 
 // Scroll to top button
 let mybutton = document.getElementById("sttB");
